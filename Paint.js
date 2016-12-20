830e2211a7966268b75c73a4b6eae09e22e56a9b
emmreb <rebeccaemmerich@gmail.com>
2016-12-20 17:57:35 +0200

    basic structure created

--- Paint.js
+++ Paint.js
@@ -1,3 +1,15 @@
-/**
- * Created by itc_user1 on 12/20/2016.
- */
+var palette=document.createElement('div');
+document.body.appendChild(palette);
+palette.className="palette";
+palette.id='b';
+
+var canvas=document.createElement('div');
+document.body.appendChild(canvas);
+canvas.className="canvas";
+canvas.id='a';
+
+for(var i=1;i<6;i++) {
+    var x = document.createElement('div');
+    document.getElementById('b').appendChild(x);
+    x.className = "color"+i;
+}
